73b560f555b45b2edae53903118b643ae4ecb55d
DenJavaExp <marmyshev.denis@mail.ru>
2021-10-23 19:22:40 +0200

    Сделал ДЗ 2

---
+++ src/HW2/HomeWork2.java
@@ -0,0 +1,71 @@
+package HW2;
+
+import java.util.Scanner;
+
+public class HomeWork2 {
+
+    public static void main(String[] args) {
+        System.out.println("Введите год: ");
+        Scanner console = new Scanner(System.in);
+        int year = console.nextInt();
+        System.out.println("Result method leapYear: " + leapYear(year));
+        System.out.println("*********************************");
+        System.out.println("Result method sum: " + sum(5, -10));
+        System.out.println("*********************************");
+        System.out.println("Result method positiveNegative: ");
+        positiveNegative(0);
+        System.out.println("*********************************");
+        System.out.println("Result method evenOdd: " + evenOdd(10));
+        System.out.println("*********************************");
+        System.out.println("Result method print: ");
+        print("Привет", 10);
+
+    }
+
+
+    public static boolean sum(int a, int b) {
+
+        int sum = a + b;
+
+        if (sum >= 10 || sum <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int positiveNegative(int a) {
+        if (a < 0) {
+            System.out.println("Число отрицаткльное");
+        } else if (a >= 0) {
+            System.out.println("Число положительное");
+        }
+        return a;
+    }
+
+    public static boolean evenOdd(int a) {
+        if (a < 0) {
+            return true;
+        } else
+            return false;
+
+
+    }
+
+    public static String print(String c, int a) {
+        for (int i = 0; i <= a; i++) {
+            System.out.println(c);
+        }
+        return c;
+    }
+
+    public static boolean leapYear(int year) {
+        if (year % 4 == 0 || year % 400 == 0) {
+            return true;
+        } else if (year % 4 != 0 || year % 100 == 0) {
+            return false;
+        }
+        return true;
+    }
+}
+
