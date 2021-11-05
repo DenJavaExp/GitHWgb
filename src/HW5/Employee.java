9e3c2bd616c09c4f208d14ddc0a1160945d5b0bb
DenJavaExp <marmyshev.denis@mail.ru>
2021-11-05 19:32:16 +0200

    -удалил ненужный метод listEmployee; -удалил лишние обьекты person; -удалил лишние getValue; -добавил цикл for each для вывода списка сотрудников при помощи метода toString();

--- src/HW5/Employee.java
+++ src/HW5/Employee.java
@@ -4,24 +4,7 @@ public class Employee {
 
     public static void main(String[] args) {
 
-
-        Person person1 = new Person("Карась Борис Григорьевич", "Инженер",
-                "Karas@mail.ru", 123456, 30000, 30);
-        Person person2 = new Person("Пескарь Антон Владимирович", "токарь",
-                "Peskar@mail.ru", 234567, 25000, 25);
-        Person person3 = new Person("Плотва Андрей Васильевич", "слесарь",
-                "Plotva@mail.ru", 345678, 25000, 45);
-        Person person4 = new Person("Язь Виталий Николаевич", "плотник",
-                "Yz@mail.ru", 456789, 23000, 56);
-        Person person5 = new Person("Лещ Петр Геннадьевич", "механик",
-                "Lesh@mail.ru", 567890, 27000, 38);
-
-
         Person[] persArray = new Person[5];
-
-        for (int i = 0; i < persArray.length; i++) {
-            persArray[i] = new Person();
-        }
 
         persArray[0] = new Person("Карась Борис Григорьевич", "Инженер",
                 "Karas@mail.ru", 123456, 30000, 30);
@@ -35,28 +18,20 @@ public class Employee {
                 "Lesh@mail.ru", 567890, 27000, 38);
 
         System.out.println("LIST OF EMPLOYEES OF THE ORGANIZATION: " + "\n");
-        person1.listEmployee();
-        System.out.println();
-        person2.listEmployee();
-        System.out.println();
-        person3.listEmployee();
-        System.out.println();
-        person4.listEmployee();
-        System.out.println();
-        person5.listEmployee();
+
+        for (Person value : persArray) {
+            System.out.println(value);
+        }
         System.out.println();
 
         System.out.println("EMPLOYEES OVER 40 YEARS OLD: " + "\n");
 
-            for (Person person : persArray) {
+        for (Person person : persArray) {
 
-                if (person.getAge() > 40)
-                    System.out.println(person + "\n");
+            if (person.getAge() > 40)
+                System.out.println(person + "\n");
 
-            }
-
-
-
+        }
     }
 }
 
--- src/HW5/Person.java
+++ src/HW5/Person.java
@@ -2,12 +2,12 @@ package HW5;
 
 public class Person {
 
-   private String name;
-   private String post;
-   private String email;
-   private int phoneNumber;
-   private double salary;
-   private int age;
+    private String name;
+    private String post;
+    private String email;
+    private int phoneNumber;
+    private double salary;
+    private int age;
 
 
     public String toString() {
@@ -22,8 +22,7 @@ public class Person {
     }
 
     public Person(String name, String post, String email,
-                  int phoneNumber, double salary, int age)
-    {
+                  int phoneNumber, double salary, int age) {
         this.name = name;
         this.post = post;
         this.email = email;
@@ -32,42 +31,7 @@ public class Person {
         this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getPost() {
-        return post;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
     public int getAge() {
         return age;
-    }
-
-    public void setAge() {
-
-    }
-
-    public void listEmployee() {
-
-        System.out.println("Name: " + getName());
-        System.out.println("Post: " + getPost());
-        System.out.println("Email: " + getEmail());
-        System.out.println("PhoneNumber: " + getPhoneNumber());
-        System.out.println("Salary: " + getSalary());
-        System.out.println("Age: " + getAge());
-
     }
 }
